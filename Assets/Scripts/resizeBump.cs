a189d21f6ef7e133cbdc9c3ae0cf4b5273d70ca3
Martin Tsang <m4rtin.t@gmail.com>
2014-10-25 14:58:24 -0700

    backup+bump

---
+++ Assets/Scripts/resizeBump.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resizeBump : MonoBehaviour {
+
+	public bool enterTrigger;
+	// Use this for initialization
+	void Start () {
+		enterTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (enterTrigger) {
+			Debug.Log ("I can change it now");
+			//Change stuff here
+			transform.localScale = new Vector3 (Random.Range (5.0f, 25.0f), Random.Range (5.0f, 25.0f), 0.0f);
+			enterTrigger=false;
+
+				}
+
+	}
+}
---
+++ Assets/Scripts/transportBackUp.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transportBackUp : MonoBehaviour {
+	float endPositionY;
+	float ballX = 43.2f;
+	float ballY;
+	public static float bottomFloorHeight = -18.4f;
+	public static float startFloorHeight = 19.6f;
+	float scale = 5.0f;
+	double oneScale;
+
+	resizeBump otherScript;
+	void Start(){
+
+		otherScript = GameObject.Find ("bump").GetComponent<resizeBump> ();
+	}
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+
+		//location
+		//Get position y of ball.
+		endPositionY = transform.position.y;
+		Debug.Log ("End Position Y: "+endPositionY);
+		//Get Height different between ball and floor
+		float height = endPositionY - bottomFloorHeight;
+		//Add Height between ball and floor to Y Starting position.
+		ballY = startFloorHeight + height;
+		//Change Position of ball up
+		transform.position = new Vector2 (ballX, ballY);
+
+		//size
+		scale = Random.Range (5.0f, 25.0f);
+		oneScale = Mathf.Round (scale * 1.000f)/1.000f ;
+
+
+		otherScript.enterTrigger = true;
+
+		}
+
+	void Update(){
+				if (transform.localScale.x != scale) {
+						if (transform.localScale.x < oneScale) {
+								transform.localScale += new Vector3 (0.1f, 0.1f, 0.0f);
+			
+						} else {
+								transform.localScale -= new Vector3 (0.1f, 0.1f, 0.0f);
+						}
+				}
+
+		}
+}
+	

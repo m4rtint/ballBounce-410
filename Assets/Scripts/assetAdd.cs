3e5e47104820d7d6579917e132512842dd204a95
Martin Tsang <m4rtin.t@gmail.com>
2014-10-25 17:08:09 -0700

    update - radius, material

---
+++ Assets/Scripts/assetAdd.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class assetAdd : MonoBehaviour {
+	/*
+	@MenuItem("GameObject/Create Material")
+	static function CreateMaterial () {
+		// Create a simple material asset
+		var material = new Material (Shader.Find("Specular"));
+		AssetDatabase.CreateAsset(material, "Assets/MyMaterial.mat");
+		// Print the path of the created asset
+		Debug.Log(AssetDatabase.GetAssetPath(material));
+	}
+	*/
+}

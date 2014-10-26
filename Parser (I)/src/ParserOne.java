147755dd07d5bf068729249cbbef851b45c36fcd
n2d8 <09orrp1@gmail.com>
2014-10-25 20:08:52 -0700

    add the first parser

---
+++ Parser (I)/src/ParserOne.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+public class ParserOne {
+	/* This parser suppose to change the outputs of the bash file output 
+	 * into the measure we use to generate our visualization which are the
+	 * parameters stated below. These should all be computable by the output
+	 * of the bash file. 
+	 */ 
+	int nOL; //number of line for each user
+	int ePU; //number of errors for each user
+	int nOC; //number of contributors in the project
+	int projSize; //project size
+	int errors; //number of errors
+	File inputFile; //file output from that the bash creates
+	
+	private void ReadFile(File inputs){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}

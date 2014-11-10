1163dc98311c12fd1c835f7f5a9148b95824945d
Ronald Lam <ronaldctlam@gmail.com>
2014-11-09 18:22:06 -0800

    Added libgdx project mining functionality, provided output from script run, documentation

--- Bash Script/checkstyleScript.sh
+++ Bash Script/checkstyleScript.sh
@@ -24,11 +24,15 @@
 #sed -i 's/\r$//' checkstyleScript.sh
 #This removes the error-causing endlines introduced when
 #editing the script in Windows
+#
+#SOURCES CONSULTED
+#http://git-scm.com/docs/git-log
+#https://en.wikipedia.org/wiki/Xargs
 
 cd team-04
 git checkout -q master
-git log --pretty="%h %an" > ../commits.txt
-echo > team4.txt
+git log --pretty="%h %ae" > ../commits.txt
+rm ../team4.txt
 while read line
 do
 IFS=' '
@@ -49,24 +53,36 @@ cd ..
 rm commits.txt
 sed -i 's/\.txt$//' team4.txt
 
-#After this point, script goes through all libgdx commits
-#Amount is huge, not for small machines
+#REASONING FOR CODE BELOW
+#After reading the documentation for git log, it was decided that the 
+#--first-parent flag would be the best fit for our purposes. It takes the first
+#parent of each commit (important when dealing with branch merging) as the previous
+#commit so we can get a "better overview when viewing the evolution of a particular
+#topic branch" (taken from the documentation). We need this because the project's 
+#size is very large and we can look at only a portion of the historical data.
 
-#cd libgdx
-#git checkout -q master
-#git log --pretty="%h" > ../commits.txt
-#while read line
-#do
-#git checkout -q "$line"
-#mkdir srcagg
-#find -name '*.java' -exec mv {} /srcagg \;
-#cd ..
-#java -jar checkstyle-6.0-all.jar -c sun_checks.xml -r libgdx/srcagg > "$line".txt
-#wc -l "$line".txt >> libgdx_checkstyle_count.txt
-#rm "$line".txt
-#echo "processing commit $line"
-#cd libgdx
-#done < ../commits.txt
-#git checkout -q master
-#cd ..
-#rm commits.txt
+cd libgdx
+git checkout -q master
+git log --pretty="%h %ae" -180 --first-parent > ../commits.txt
+rm libgdx.txt
+rm -rf ../srcagg
+mkdir ../srcagg
+while read line
+do
+array=($line)
+git checkout -q "${array[0]}"
+find . -type f -name '*.java' -exec cp {} ../srcagg/ \;
+cd ..
+java -jar checkstyle-6.0-all.jar -c sun_checks.xml -r srcagg > "$line".txt
+cd libgdx
+((find ./ -name '*.java' -print0 | xargs -0 cat) | wc -l) >> ../libgdx.txt
+cd ..
+wc -l "$line".txt >> libgdx.txt
+rm "$line".txt
+echo "processing commit $line"
+cd libgdx
+done < ../commits.txt
+git checkout -q master
+cd ..
+rm commits.txt
+sed -i 's/\.txt$//' libgdx.txt

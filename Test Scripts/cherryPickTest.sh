e7884ba5685a4e7dbf235dad223ba7194fe6a6d1
Ronald Lam <ronaldctlam@gmail.com>
2014-11-16 10:40:09 -0800

    Added run instrunctions and expected output to test script

--- Test Scripts/cherryPickTest.sh
+++ Test Scripts/cherryPickTest.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
-#Script selects a few commits to test the code used in checkstyleScript.sh
+# Script selects a few commits to test the code used in checkstyleScript.sh
 #
-#RUN DIRECTIONS
-#Same as checkstyleScript.sh
-#If editing file in Windows, run
-#sed -i 's/\r$//' cherryPickTest.sh
-#./cherryPickTest.sh
+# RUN PRE-CONDITIONS
+# Put this script in directory containing the Team-04 project and WalkingPaws
+# project.
 #
-#WHAT WE WANT
-#Numbers that match, this tests whether the count of java lines in the project is
-#the same as the number we get from the folder holding the copied files.
-#NOTE THAT SO FAR THIS FAILS!
+# RUN DIRECTIONS
+# Same as checkstyleScript.sh
+# If editing file in Windows, run
+# sed -i 's/\r$//' cherryPickTest.sh
+# ./cherryPickTest.sh
+#
+# EXPECTED OUTPUT
+# The script goes into a particular commit and copies all the java files into
+# a particular directory. It then prints out the number of Java lines found
+# and the number of files copied. It then goes into the copy target directory
+# to print out the same numbers there.
+# WE WANT TWO PAIRS OF MATCHING NUMBERS PER TEST
 
-cd libgdx
-git checkout -q f21c838
+cd team-04
+git checkout -q b2009d0
 rm -rf ../srcagg
 mkdir ../srcagg
 find . -type f -name '*.java' -exec cp {} ../srcagg/ \;
 #print number of lines of java code in project
 find . -type f -name '*.java' -print0 | xargs -0 cat | wc -l
+find . -type f -name '*.java' | wc -l
 cd ../srcagg
 #print number of lines of java code copied into directory
 find . -type f -name '*.java' -print0 | xargs -0 cat | wc -l
+find . -type f -name '*.java' | wc -l
+cd ..
+
+# Test separation
+echo
+
+cd walkingpaws
+git checkout -q e173cb8
+rm -rf ../srcagg
+mkdir ../srcagg
+find . -type f -name '*.java' -exec cp {} ../srcagg/ \;
+#print number of lines of java code in project
+find . -type f -name '*.java' -print0 | xargs -0 cat | wc -l
+find . -type f -name '*.java' | wc -l
+cd ../srcagg
+#print number of lines of java code copied into directory
+find . -type f -name '*.java' -print0 | xargs -0 cat | wc -l
+find . -type f -name '*.java' | wc -l
+cd ..
